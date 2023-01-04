f6dfbc7151ee85f1abb0be5702b2cb72bea53ae1
dev-from-cbd <veryfatwombat@gmail.com>
2023-01-04 14:54:53 +1100

    Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-module.exports = function reverse(n) {
-  reverse(123); // 321
-  reverse(233); // 332
-  reverse(535); // 535
-  reverse(95034); // 43059
+module.exports = function reverse(reverseIntN) {
+  return Number(String(Math.abs(reverseIntN)).split("").reverse().join(""));
 };
